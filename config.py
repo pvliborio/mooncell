cb21ab80acbec7527499d5f75a6007b3f5780561
LeeTgk <kevin_araujo13@hotmail.com>
2021-09-17 11:39:39 -0300

    my config

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 SERVER_PORT = 8000
-ROOT_DIR = "/mnt/d/PUC/INF1407 - PROGRAMACAO PARA A WEB/mooncell"
+ROOT_DIR = "/mnt/c/Users/losma/OneDrive/Documentos/mooncell"
 ERROR_PAGE = "404.html"
 DEFAULT_FILES = ["index.html"]
 MESSAGE_NOT_SUPPORTED = "Tipo de arquivo não suportado"
+
